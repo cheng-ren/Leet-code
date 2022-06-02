92b28112a61a8660d9e9c1a67d1cc13f30b446c6
任成 <rencheng@hulai.com>
2022-06-02 17:24:33 +0800

    init project

---
+++ Leet-code/Leet-code/Other/RCObject.h
@@ -0,0 +1,17 @@
+//
+//  RCObject.h
+//  Leet-code
+//
+//  Created by 任成 on 2022/2/22.
+//  Copyright © 2022 任成. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Leet-code/Leet-code/Other/RCObject.m
@@ -0,0 +1,17 @@
+//
+//  RCObject.m
+//  Leet-code
+//
+//  Created by 任成 on 2022/2/22.
+//  Copyright © 2022 任成. All rights reserved.
+//
+
+#import "RCObject.h"
+
+@implementation RCObject
+
++ (void)load {
+    sleep(1000);
+}
+
+@end
